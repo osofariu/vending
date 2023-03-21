54dc8a2439c95c4a50231d4fc05c5bbecee7fc97
Ovi Sofariu <ovi@sofariu.com>
2023-03-21 08:33:42 -0400

    project structure, initial set

---
+++ src/test/java/learn/katas/AppTest.java
@@ -0,0 +1,20 @@
+package learn.katas;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
